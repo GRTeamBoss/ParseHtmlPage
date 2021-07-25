828ced69adbbf504563de637e8b51c22f15507f2
GR_Team_Boss <rabbitwow97@gmail.com>
2021-07-26 02:55:27 +0500

    add websockets and asyncio

--- parse.py
+++ parse.py
@@ -1,16 +1,22 @@
 #usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from os import link
+import asyncio as aio
+
 import requests as req
+import websockets as web
+
+
+__all__ = (aio.__all__)
 
 class Parse:
 
-
 	URI = 'https://www.'
+	WSS_URI = 'wss://www.'
+	WEB_CALL = web
 
-    #  CONSTRUCTOR
-	def __init__(self, GET_MODE = 'text') -> None:
+	# CONSTRUCTOR
+	def __init__(self, GET_MODE='text') -> None:
 
 		self.GET_MODE = GET_MODE
 
@@ -20,10 +26,11 @@ class Parse:
 		callback_function = {
 			'1': self.get_URL_content,
 			'2': self.get_PATH_content,
+			'3': self.get_WSS_content,
 		}
-		return callback_function[method](url = link)
+		return callback_function[method](url=link)
 
-    # CONTENT_MODIFY
+	# CONTENT_MODIFY
 	def content_modify(self, arg) -> list:
 		data_modify = list()
 
@@ -32,76 +39,42 @@ class Parse:
 
 		return data_modify
 
-    # FIND_TAG
+	# FIND_TAG
 	def find_tag(self, data: list, tag: str) -> list:
 		data_tag = list()
 		for num in range(2):
 			data_tag.append(list())
 		data_find = list()
-		data_find_dict = dict()
-		tree = dict()
-
-		if any( ( filter(lambda elem: f'<{tag}>' in elem, data), filter(lambda elem: f'<{tag}' in elem, data) ) ):
-
-			if bool(filter(lambda elem: f'</{tag}>' in elem, data)) == False:
-
-				try:
-
-					for num in range(len(data)):
-
-						if f'<{tag}' in data[num]:
-							data_tag[0].append(num)
-							data_tag[1].append(data[num])
-
-					for num in range(len(data_tag[0])):
-
-						data_find.append(f'{data[int(data_tag[0][num])]}>')
-						data_find_dict[str(data_tag[0][num])] = f'{data[int(data_tag[0][num])]}>'
-					return data_find
-
-				except Exception as e:
-					return e
-
-			else:
-
-				try:
-
-					for num in range(len(data)):
-
-						if any(( f'<{tag}' in data[num], f'</{tag}' in data[num] )):
-							data_tag[0].append(num)
-							data_tag[1].append(data[num])
-
-					for num in range(len(data_tag[0])):
-
-						# data_find.append(f'{data[int(data_tag[0][num])]}>')
-						data_find_dict[str(data_tag[0][num])] = f'{data[int(data_tag[0][num])]}>'
-
-					_temp_key = list()
-					def sort(data: dict, tree: dict, count: list, content: list) -> dict:
-						for key, value in data_find_dict.items():
-							if f'<{tag}' in value:
-								for keys in data_find_dict:
-									if f'</{tag}' in data[keys] \
-										and list(data.values()).index(data[keys]) - list(data.values()).index(value) >= 1 \
-										and len(list(filter(lambda elem: keys == elem, count))) == 0 \
-										and len(list(filter(lambda elem: key == elem, tree))) == 0:
-										tree[key] = f'{value} {"> ".join(content[int(key)+1 : int(keys)])}> {data[keys]}'
-										count.append(keys)
-						return tree
-					result = sort(data_find_dict, tree, _temp_key, data)
-					
-					# data_start = list(filter(lambda elem: f'<{tag}' in elem or f'<{tag}>' in elem, data_find))
-					# data_end = list(filter(lambda elem: f'</{tag}>' in elem, data_find))
-					return result
-
-				except Exception as e:
-					return e
+		count_start = int()
+		count_end = int()
+		if bool(filter(lambda elem: f'<{tag}' in elem, data)):
+			for element in data:
+				if any((f'{tag}' in element, f'/{tag}' in element)):
+					data_tag[0].append([
+						data.index(element),
+						f'start{count_start}' if f'{tag}' in element and
+						f'/{tag}' not in element else f'end{count_end}'
+					])
+					data_tag[1].append(f'{element}>')
+					if f'{tag}' in element and f'/{tag}' not in element:
+						count_start += 1
+					if f'/{tag}' in element:
+						count_end += 1
+			count = 1
+			while len(data_find) < len(data_tag[1])//2:
+				for num in range(len(data_tag[0])):
+					if num < len(data_tag[0])-count:
+						_now = data_tag[0][num][1]
+						_next = data_tag[0][num+count][1]
+						if 'start' in _now and 'end' in _next:
+							data_find.append([f'{data_tag[1][num]}', f'{data_tag[1][num+count]}'])
+				count += 1
+			return data_find
 		else:
 			return 'This tag not found!'
 
 
-    # GET_URL_CONTENT
+	# GET_URL_CONTENT
 	def get_URL_content(self, url) -> list:
 
 		RESPONSE = req.get(f'{self.URI}{url}')
@@ -111,23 +84,33 @@ class Parse:
 		}
 
 		data = MODE[self.GET_MODE].rstrip().split('>')
-		data_modified = self.content_modify(arg = data)
+		data_modified = self.content_modify(arg=data)
 		return data_modified
 
-    # GET_PATH_CONTENT
+	# GET_PATH_CONTENT
 	def get_PATH_content(self, path) -> list:
 
 		with open(f'{path}', 'r', encoding='utf-8') as file:
 			RESPONSE = file.read()
 
-		data = RESPONSE.rstrip().split('\n')
-		data_modified = self.content_modify(arg = data)
+		data = RESPONSE.rstrip().split('>')
+		data_modified = self.content_modify(arg=data)
 		return data_modified
 
 
+	async def get_WSS_content(self, url) -> list:
+		web_connect = self.WEB_CALL.connect(f'{self.WSS_URI}{url}')
+		data = web_connect.recv().rstrip().split('>')
+		data_modified = self.content_modify(arg=data)
+		web_connect.close()
+		return data_modified
+		
+		
+		
 
 
-parse = Parse().call_method('1', 'instagram.com/gr_team_boss/?__a=1')
-# print(parse)
-tag = Parse().find_tag(parse, 'div')
-print(tag)
+
+
+parse = Parse().call_method('3', 'instagram.com/gr_team_boss/?__a=1')
+# tag = Parse().find_tag(parse, 'div')
+print(parse)
